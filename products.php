8ef6cd017497222ff7f9772f130a745e44d2345e
Fat Nela <fatnela94@gmail.com>
2020-05-22 22:49:24 +0200

    Design Addition - Complete CSS

---
+++ products.php
@@ -0,0 +1,144 @@
+<?php include ('config.php'); ?>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Elefanti60</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/media.css">
+</head>
+<body>
+    <?PHP
+        include"includes/header.php";
+    ?>
+<div class="wrapper">
+
+    <div class="content">
+        <!--<hr class="divider">-->
+        <div class="products-wrapper">
+            <h2>Products</h2>
+
+
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/monitors.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Monitors</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 159.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="product out-of-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+    </div>
+
+</div>
+<footer>
+    <p>All rights reserved "Elefanti60" &#169; 2020.</p>
+</footer>
+<script type="text/javascript" src="./js/main.js"></script>
+</body>
+</html>
