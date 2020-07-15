eae8e974fa7f4eb710a45ddafb233cf4fa18ba3b
Lorik Haxhidauti <lorikhaxhidauti@gmail.com>
2020-07-15 21:48:26 +0200

    Revert "Lorik Haxhidauti - Final changes"
    This reverts commit 43e961e90bb9d5ad333bce5b0772d5c639789bef.

--- products.php
+++ products.php
@@ -1,12 +1,4 @@
-<?php 
-
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-include $root."/Controller/ProductController.php";
-require $root."/Model/ProductModel.php";
-$ProductController=new ProductController();
-$products=$ProductController->getAllProducts();
-
-?>
+<?php include ('config.php'); ?>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,47 +17,128 @@ $products=$ProductController->getAllProducts();
         <!--<hr class="divider">-->
         <div class="products-wrapper">
             <h2>Products</h2>
-            <?php              
-             $count=0;
-             foreach($products as $item){
-                     $count++;
-                     if($item->get_nostock()==0){
-                         $class = "out-of-stock";
-                     }else
-                     {
-                         $class = "in-stock";
-             
-                     }
-                     
-                     echo '<div class="product '.$class.'"><a href="product.php?id='.$item->get_id().'" style = "background-color: unset !important; color: unset !important; text-decoration:none !important; ">
-                     <div class="product-image">
-                         <img class = "image100" src="'. $item->get_image().'" alt="">
-                     </div>
-                     <hr class="product_divider">
-                     <div class="product-details">
-                         <div class="title">
-                             <h4>'. $item->get_name().'</h4>
-                         </div>
-                         <div class="price">
-                             <p>Price: '. $item->get_price() .'&#8364;</p>
-                             <p>'. $item->get_nostock() .' left in stock.</p>
-                         </div>
-                     </div>
-                     <div class="product_clone">
-                     </div></a>
-                 </div>';
-             }
-             if($count==0){
-                 echo "0 results";
-             }
-            ?>
+
+
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/monitors.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Monitors</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 159.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/laptops.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Laptops</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 499.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="product in-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="product out-of-stock">
+                <div class="product-image">
+                    <img src="./images/phones.png" alt="">
+                </div>
+                <hr class="product_divider">
+                <div class="product-details">
+                    <div class="title">
+                        <h4>Phones</h4>
+                    </div>
+                    <div class="price">
+                        <p>Starting from: 179.99&#8364;</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
     </div>
     </div>
 
 </div>
-<?PHP
-include "includes/footer.php";
-?>
+<footer>
+    <p>All rights reserved "Elefanti60" &#169; 2020.</p>
+</footer>
 <script type="text/javascript" src="./js/main.js"></script>
 </body>
 </html>
